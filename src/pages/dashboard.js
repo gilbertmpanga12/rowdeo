e150b4d31c6dad6a70409bba2db5d62ffbb56cf8
Benjamin Kalungi <benkalungi256@gmail.com>
2021-09-28 09:06:04 +0000

    FEAT(firebase-storage): Adds file upload functionality

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useAuth } from 'reactfire';
 import { Redirect } from 'react-router-dom'
+import { useDropzone } from 'react-dropzone'
 
-
-export default function Dashboard(){
-    const signOut = auth => auth.signOut().then(() => <Redirect to='/'/>);
+export default function Dashboard() {
     const auth = useAuth();
-       return(
-           <div className="dashboard">
-                <h3>Dashboard...</h3>
-                <button onClick={()=>signOut(auth)}>
-                    Signout
-                </button>
-           </div>
-       )
+    const signOut = auth => auth.signOut().then(() => <Redirect to='/' />);
+    const onDrop = useCallback(
+        acceptedFiles => {
+            //upload the files to firebase storage...
+        }, []
+    )
+    const { getInputProps, getRootProps, isDragActive } = useDropzone({ onDrop });
+    return (
+        <div className="dashboard flex">
+            <h3>Dashboard...</h3>
+            <button onClick={() => signOut(auth)}>
+                Signout
+            </button>
+
+            <div className="dropzone"  {...getRootProps()}>
+                <input {...getInputProps()} />
+                {
+                    isDragActive ?
+                        <p>Drop the files here ...</p> :
+                        <p>Drag 'n' drop some files here, or click to select files</p>
+                }
+            </div>
+                {/**
+                 * Button toggles the upload form
+                 */}
+            <button className="flex" onClick={``}>
+                <span className="">
+                    Upload...
+                    </span>
+            </button>
+        </div>
+    )
 }
